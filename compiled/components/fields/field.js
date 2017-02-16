e70d196aa4e571b6c7ac35fd57d82d4e4fd42a47
Matanya Fischeimer <matfish@gmail.com>
2017-02-16 10:46:10 +0200

    fix

--- compiled/components/fields/field.js
+++ compiled/components/fields/field.js
@@ -1,17 +1,26 @@
 'use strict';
 
+var _field = require('../../templates/field');
+
+var _field2 = _interopRequireDefault(_field);
+
+var _watch = require('../mixins/watch');
+
+var _watch2 = _interopRequireDefault(_watch);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
 var props = require('../mixins/props');
 var data = require('../mixins/data');
 var methods = require('../mixins/methods');
 var computed = require('../mixins/computed');
 var mounted = require('../mixins/mounted');
-var field = require('../../templates/field');
-var watch = require('../mixins/watch');
+
 
 module.exports = function () {
 	return {
-		render: field,
-		mixins: [props, data, methods, computed, mounted, watch],
+		render: _field2.default,
+		mixins: [props, data, methods, computed, mounted, _watch2.default],
 		methods: {
 			setValue: function setValue(value) {
 				this.curValue = value;
