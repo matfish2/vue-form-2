e7d74de4f7d23944165b2dc78e5f42c50e3eaffb
Matanya Fischeimer <matfish@gmail.com>
2017-02-21 14:01:20 +0200

    remove error when field does not exist

--- compiled/methods/show-all-errors.js
+++ compiled/methods/show-all-errors.js
@@ -1,12 +1,21 @@
 "use strict";
 
 module.exports = function () {
-  this.errors.forEach(function (error) {
-    var field = this.getField(error.name);
-    if (field.errors.indexOf(error.rule) == -1) {
-      field.errors.push(error.rule);
-      field.hadErrors = true;
-    }
-    error.show = true;
-  }.bind(this));
+
+	var errors = [];
+
+	this.errors.forEach(function (error) {
+		var field = this.getField(error.name);
+
+		if (field) {
+			if (field.errors.indexOf(error.rule) == -1) {
+				field.errors.push(error.rule);
+				field.hadErrors = true;
+			}
+			error.show = true;
+			errors.push(error);
+		}
+	}.bind(this));
+
+	this.errors = errors;
 };
