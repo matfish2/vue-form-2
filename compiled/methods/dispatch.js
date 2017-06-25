59617c3f9c018800594d0ee8d4cae17f3ed49930
Matanya Fischeimer <matfish@gmail.com>
2017-06-25 14:21:44 +0300

    fire event bus even when using vuex

--- compiled/methods/dispatch.js
+++ compiled/methods/dispatch.js
@@ -10,16 +10,13 @@ module.exports = function (event, payload) {
 	var pieces = event.split("::");
 	var eventName = pieces[0];
 
-	if (this.vuex) {
-
-		if (pieces.length > 1) return;
-
-		eventName = eventName.toUpperCase().replace('-', '_');
-		this.$store.commit(this.name + '/' + eventName, payload);
-	} else {
-		pieces[0] = this.name ? this.name + '.' + eventName : eventName;
-		event = "vue-form." + pieces.join('::');
-
-		_bus2.default.$emit(event, payload);
+	if (this.vuex && pieces.length === 1) {
+		var EventName = eventName.toUpperCase().replace('-', '_');
+		this.$store.commit(this.name + '/' + EventName, payload);
 	}
+
+	pieces[0] = this.name ? this.name + '.' + eventName : eventName;
+	event = "vue-form." + pieces.join('::');
+
+	_bus2.default.$emit(event, payload);
 };
