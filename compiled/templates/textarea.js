10dfb05076b217741a7e648f24762eab793dc542
Matanya <matfish2@gmail.com>
2017-10-08 12:57:28 +0300

    use v-model-jsx to fix textarea binding

--- compiled/templates/textarea.js
+++ compiled/templates/textarea.js
@@ -1,44 +1,62 @@
 "use strict";
 
 Object.defineProperty(exports, "__esModule", {
-        value: true
+      value: true
 });
 
+var _babelHelperVueJsxMergeProps = require("babel-helper-vue-jsx-merge-props");
+
+var _babelHelperVueJsxMergeProps2 = _interopRequireDefault(_babelHelperVueJsxMergeProps);
+
 exports.default = function (h) {
+      var _this = this;
 
-        var button = this.toggler ? h(
-                "button",
-                {
-                        attrs: { type: "button" },
-                        "class": "btn btn-default btn-xs", on: {
-                                "click": this.toggle.bind(this)
-                        }
-                },
-                [this.togglerText]
-        ) : '';
+      var button = this.toggler ? h(
+            "button",
+            {
+                  attrs: { type: "button" },
+                  "class": "btn btn-default btn-xs", on: {
+                        "click": this.toggle.bind(this)
+                  }
+            },
+            [this.togglerText]
+      ) : '';
 
-        return h(
-                "div",
-                { "class": "Textarea__wrapper" },
-                [h(
-                        "textarea",
-                        {
-                                attrs: {
-                                        name: this.name,
-                                        id: "textarea_" + this.name,
-
-                                        disabled: this.disabled,
-                                        placeholder: this.placeholder },
-                                "class": "form-control",
-                                on: {
-                                        "change": this.updateValue.bind(this),
-                                        "keyup": this.updateValue.bind(this)
-                                },
-                                domProps: {
-                                        "value": this.curValue
-                                }
+      return h(
+            "div",
+            { "class": "Textarea__wrapper" },
+            [h(
+                  "textarea",
+                  (0, _babelHelperVueJsxMergeProps2.default)([{
+                        attrs: {
+                              name: this.name,
+                              id: "textarea_" + this.name
                         },
-                        [this.value]
-                ), button]
-        );
+                        "class": "form-control",
+                        on: {
+                              "change": this.updateValue.bind(this),
+                              "keyup": this.updateValue.bind(this),
+                              "input": function input($event) {
+                                    if ($event.target.composing) return;
+                                    _this.curValue = $event.target.value;
+                              }
+                        },
+                        domProps: {
+                              "value": _this.curValue
+                        }
+                  }, {
+                        directives: [{
+                              name: "model",
+                              value: _this.curValue
+                        }]
+                  }, {
+                        attrs: {
+                              disabled: this.disabled,
+                              placeholder: this.placeholder }
+                  }]),
+                  []
+            ), button]
+      );
 };
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
