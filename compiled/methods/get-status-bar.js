1cb12e6951ea15332e1e30bae8642c68b8a16733
Matanya <matfish2@gmail.com>
2017-11-03 08:34:49 +0000

    allow no status bar (#21)

--- compiled/methods/get-status-bar.js
+++ compiled/methods/get-status-bar.js
@@ -1,14 +1,20 @@
 "use strict";
 
 module.exports = function () {
-  return getStatusbar(this.$parent);
+	return getStatusbar(this.$parent);
 };
 
 function getStatusbar(instance) {
 
-  if (!instance) return false;
+	if (!instance) return {
+		danger: function danger() {},
+		success: function success() {},
+		info: function info() {},
+		warning: function warning() {},
+		reset: function reset() {}
+	};
 
-  if (instance.$refs && instance.$refs.statusbar) return instance.$refs.statusbar;
+	if (instance.$refs && instance.$refs.statusbar) return instance.$refs.statusbar;
 
-  return getStatusbar(instance.$parent);
+	return getStatusbar(instance.$parent);
 }
