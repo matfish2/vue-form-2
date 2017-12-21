d0747cc4d6c37abaa449e255787e7fdc29674b70
Matanya <matfish2@gmail.com>
2017-12-21 12:48:49 +0200

    add timeout to client submit

--- compiled/methods/submitters/client.js
+++ compiled/methods/submitters/client.js
@@ -4,10 +4,14 @@ module.exports = function (vm) {
   return {
     submit: function submit(e) {
       if (e) e.preventDefault();
-      var data = vm.formData();
-      vm.reinitForm();
-      vm.dispatch('sent', data);
-      vm.getStatusBar().success(vm.opts.texts.sent);
+
+      setTimeout(function () {
+        var data = vm.formData();
+        vm.reinitForm();
+        vm.dispatch('sent', data);
+        vm.getStatusBar().success(vm.opts.texts.sent);
+      }, 50);
+
       return true;
     }
   };
