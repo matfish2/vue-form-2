42089634302116cda7e874e61ec16cb49ffa1b37
Matanya <matfish2@gmail.com>
2018-03-20 13:45:17 +0000

    fix

--- compiled/helpers/is-hidden.js
+++ compiled/helpers/is-hidden.js
@@ -1,6 +1,6 @@
 "use strict";
 
 module.exports = function () {
-    var elem = that.$el;
+    var elem = this.$el;
     return !(elem.offsetWidth || elem.offsetHeight || elem.getClientRects().length);
 };
