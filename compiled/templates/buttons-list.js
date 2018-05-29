21ffb3dfcf38fbdb7722ec6df95d3c579fe8e2a7
matfish2 <matfish2@gmail.com>
2018-05-29 13:25:34 +0100

    remove  toggler from disabled buttons list

--- compiled/templates/buttons-list.js
+++ compiled/templates/buttons-list.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 Object.defineProperty(exports, "__esModule", {
   value: true
@@ -10,22 +10,24 @@ exports.default = function (h) {
   var toggler = '';
   var items = [];
 
-  if (this.multiple) {
+  if (this.disabled) {
+    toggler = '';
+  } else if (this.multiple) {
     toggler = h(
-      "span",
-      { "class": "pull-right btn btn-link",
+      'span',
+      { 'class': 'pull-right btn btn-link',
         on: {
-          "click": this.toggle.bind(this)
+          'click': this.toggle.bind(this)
         }
       },
       [this.toggleText]
     );
   } else {
     toggler = this.curValue ? h(
-      "span",
-      { "class": "pull-right btn btn-link",
+      'span',
+      { 'class': 'pull-right btn btn-link',
         on: {
-          "click": this.clear.bind(this)
+          'click': this.clear.bind(this)
         }
       },
       [this.clearText]
@@ -34,15 +36,15 @@ exports.default = function (h) {
 
   this.items.map(function (item) {
     if (_this.passesFilter(item)) items.push(h(
-      "div",
-      { "class": _this.itemClass },
+      'div',
+      { 'class': _this.itemClass },
       [h(
-        "label",
-        { "class": "form-check-label" },
+        'label',
+        { 'class': 'form-check-label' },
         [h(
-          "input",
+          'input',
           {
-            "class": "form-check-input",
+            'class': 'form-check-input',
             attrs: { disabled: _this.disabled,
               name: _this.Name + _this.arraySymbol,
               type: _this.type,
@@ -50,13 +52,13 @@ exports.default = function (h) {
 
               checked: _this.isChecked(item.id) },
             on: {
-              "change": _this.updateValue.bind(_this, item.id)
+              'change': _this.updateValue.bind(_this, item.id)
             }
           },
           []
         ), h(
-          "span",
-          { "class": "form-check-label-text" },
+          'span',
+          { 'class': 'form-check-label-text' },
           [item.text]
         )]
       )]
@@ -66,8 +68,8 @@ exports.default = function (h) {
   var content = items.length ? [toggler, items] : [this.getForm().opts.texts.noItems];
 
   return h(
-    "div",
-    { "class": "VF-Buttons__wrapper" },
+    'div',
+    { 'class': 'VF-Buttons__wrapper' },
     [content]
   );
 };
