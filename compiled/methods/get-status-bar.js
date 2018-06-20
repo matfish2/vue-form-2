c9fa7e9088ac76b714657d455797c25ad166cf27
matfish2 <matfish2@gmail.com>
2018-06-20 10:50:22 +0100

    fix statusbar getter

--- compiled/methods/get-status-bar.js
+++ compiled/methods/get-status-bar.js
@@ -1,20 +1,7 @@
-"use strict";
+'use strict';
 
 module.exports = function () {
-	return getStatusbar(this.$parent);
+	return this.$children.find(function (child) {
+		return child.$options.name === 'status-bar';
+	});
 };
-
-function getStatusbar(instance) {
-
-	if (!instance) return {
-		danger: function danger() {},
-		success: function success() {},
-		info: function info() {},
-		warning: function warning() {},
-		reset: function reset() {}
-	};
-
-	if (instance.$refs && instance.$refs.statusbar) return instance.$refs.statusbar;
-
-	return getStatusbar(instance.$parent);
-}
